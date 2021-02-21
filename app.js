a417c1f00325dae976eb9bce7cc90dee1be35aef
AmalElkady <AmalElkady@users.noreply.github.com>
2021-02-21 21:20:21 +0200

    change-default-page

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
-const mongoose = require("mongoose")
-mongoose.Promise = require("bluebird")
-const path = require('path')
+const mongoose = require("mongoose");
+mongoose.Promise = require("bluebird");
+const path = require("path");
 
-const express = require('express')
-const app = express()
-const server = require('http').Server(app)
-const socket = require('socket.io')(server)
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+const socket = require("socket.io")(server);
 
-
-const port = 8080
+const port = 8080;
 
 // database connection
-const connect = require("./dbconnect")
+const connect = require("./dbconnect");
 
 // frontend thing
-app.use(express.static(path.join(__dirname, "public")))
+app.use(
+  express.static(path.join(__dirname, "public"), { index: "login.html" })
+);
 
 // const chatRouter = require("./chat.route")
 // app.use("/chats", chatRouter)
@@ -22,35 +23,34 @@ app.use(express.static(path.join(__dirname, "public")))
 // const loginRouter = require("./login.route")
 // app.use("/login", loginRouter)
 
-
 // server and socket -- setup event listener
 socket.on("connection", socket => {
-    console.log("user connected")
-    // check if user got messages or not to send
+  console.log("user connected");
+  // check if user got messages or not to send
 
-    socket.on("disconnect", function () {
-        console.log("user disconnected")
-    })
+  socket.on("disconnect", function() {
+    console.log("user disconnected");
+  });
 
-    // //Someone is typing
-    // socket.on("typing", data => {
-    //     socket.broadcast.emit("notifyTyping", {
-    //         user: data.user,
-    //         message: data.message
-    //     })
-    // })
+  // //Someone is typing
+  // socket.on("typing", data => {
+  //     socket.broadcast.emit("notifyTyping", {
+  //         user: data.user,
+  //         message: data.message
+  //     })
+  // })
 
-    // //when soemone stops typing
-    // socket.on("stopTyping", () => {
-    //     socket.broadcast.emit("notifyStopTyping")
-    // })
+  // //when soemone stops typing
+  // socket.on("stopTyping", () => {
+  //     socket.broadcast.emit("notifyStopTyping")
+  // })
 
-    socket.on("chat message", function (msg) {
-        // the user will send his own ID, reciever ID and msg
-        // 
-    })
-})
+  socket.on("chat message", function(msg) {
+    // the user will send his own ID, reciever ID and msg
+    //
+  });
+});
 
-server.listen(port, function () {
-    console.log('Listening on port ' + port)
-})
+server.listen(port, function() {
+  console.log("Listening on port " + port);
+});
